157c566c940a4a75e88e8a99dd9d55d4c96698c4
biggybi <biggybi@pm.me>
2026-02-12 02:49:21 +0100

    fix(wezterm): wrong theme on switch from dark to light the first time

--- shells/wezterm/colorscheme.lua
+++ shells/wezterm/colorscheme.lua
@@ -1,16 +1,21 @@
 local wezterm = require("wezterm")
 
-local function get_appearance()
-  if wezterm.gui then return wezterm.gui.get_appearance() end
-  return "Dark"
-end
+local themes = {
+  Default = "Catppuccin Latte",
+  Light = "Catppuccin Latte",
+  Dark = "Catppuccin Mocha",
+  LightHighContrast = "Catppuccin Latte",
+  DarkHighContrast = "Catppuccin Mocha",
+}
 
-local function scheme_for_appearance(appearance)
-  if appearance:find("Dark") then
-    return "Catppuccin Mocha"
-  else
-    return "Catppuccin Latte"
+local function change_theme(window)
+  local overrides = window:get_config_overrides() or {}
+  local appearance = window:get_appearance()
+  local scheme = themes[appearance] or themes.default
+  if overrides.color_scheme ~= scheme then
+    overrides.color_scheme = scheme
+    window:set_config_overrides(overrides)
   end
 end
 
-return { colorscheme = scheme_for_appearance(get_appearance()) }
+wezterm.on("window-config-reloaded", change_theme)
--- shells/wezterm/config.lua
+++ shells/wezterm/config.lua
@@ -3,7 +3,7 @@ local wezterm = require("wezterm")
 local M = wezterm.config_builder() or {}
 
 M = {
-  color_scheme = require("colorscheme").colorscheme,
+  color_scheme = "Catppuccin Mocha",
   color_schemes = require("catppuccin"),
   enable_wayland = true,
   line_height = 1.3,
