f6a8629b55c16b3d627e62b81405ac56e9f89bb4
biggybi <biggybi@pm.me>
2026-06-21 21:04:02 +0200

    feat(wezterm): schemes: catppuccin_latte: true white background

--- shells/wezterm/catppuccin.lua
+++ shells/wezterm/catppuccin.lua
@@ -1,10 +1,14 @@
 local wezterm = require("wezterm")
 
-local schemes = wezterm.get_builtin_color_schemes()
-local catppuccin_mocha = schemes["Catppuccin Mocha"]
+local schemes = wezterm.color.get_builtin_schemes()
 
+local catppuccin_mocha = schemes["Catppuccin Mocha"]
 catppuccin_mocha.background = "#0b0f14"
+
+local catppuccin_latte = schemes["Catppuccin Latte"]
+catppuccin_latte.background = "#ffffff"
 
 return {
   ["Catppuccin Mocha"] = catppuccin_mocha,
+  ["Catppuccin Latte"] = catppuccin_latte,
 }
